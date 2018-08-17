607c22f473595ec6b254123f006aded360d339f9
Jakub Mikians <jakub.mikians@gmail.com>
2018-08-17 23:27:06 +0200

    Dirty (not working) code for echo with distance detector
    Mixed with some RTC code.

--- src/main.c
+++ src/main.c
@@ -1,14 +1,23 @@
 #include "FreeRTOS.h"
 #include "logger.h"
 #include "main.h"
+#include "stm32f4xx_hal_rtc.h"
 #include "task.h"
 
 #define ledSTACK_SIZE	configMINIMAL_STACK_SIZE
 #define ledPRIORITY		( tskIDLE_PRIORITY + 1UL )
 
+#define RTC_ASYNCH_PREDIV  0x7F   /* LSE as RTC clock */
+#define RTC_SYNCH_PREDIV 0x00FF /* LSE as RTC clock */
+
 static void Error_Handler(void);
 static void SystemClock_Config(void);
-static void vLEDFlashTask(void *pvParameters);
+static void vTriggerMeasurementTask(void *pvParameters);
+static void vMeasureTask(void *pvParameters);
+static void RTC_Config(void);
+
+void configureGpioLed();
+void configureGpioDistance();
 
 int main(void)
 {
@@ -27,20 +36,16 @@ int main(void)
 
 	/* Configure the System clock to 84 MHz */
 	SystemClock_Config();
-
-	GPIO_InitTypeDef GPIO_InitStruct;
-
+	loggerPrintf("test logger");
+	RTC_Config(); 
 	__GPIOA_CLK_ENABLE();
 
-	/* Configure PA05 IO in output push-pull mode to drive external LED */
-	GPIO_InitStruct.Pin = GPIO_PIN_5;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_PULLUP;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+	configureGpioLed();
+	configureGpioDistance();
 
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-	xTaskCreate(vLEDFlashTask, "LED1", ledSTACK_SIZE, (void *)NULL,
+	xTaskCreate(vTriggerMeasurementTask, "MEAS1", ledSTACK_SIZE,
+		    (void *)NULL, ledPRIORITY, (TaskHandle_t *) NULL);
+	xTaskCreate(vMeasureTask, "MEAS2", ledSTACK_SIZE, (void *)NULL,
 		    ledPRIORITY, (TaskHandle_t *) NULL);
 
 	vTaskStartScheduler();
@@ -51,18 +56,83 @@ int main(void)
 	}
 }
 
-static void vLEDFlashTask(void *pvParameters)
+void configureGpioLed()
 {
-	int i = 0;
+	/* Configure PA05 IO in output push-pull mode to drive external LED */
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.Pin = GPIO_PIN_5;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+#define GPIO_PIN_DIST_TRIGGER	GPIO_PIN_6
+#define GPIO_PIN_DIST_ECHO 	GPIO_PIN_7
+void configureGpioDistance()
+{
+	{
+		GPIO_InitTypeDef GPIO_InitStruct;
+		GPIO_InitStruct.Pin = GPIO_PIN_DIST_TRIGGER;
+		GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+		GPIO_InitStruct.Pull = GPIO_PULLUP;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	}
+	{
+		GPIO_InitTypeDef GPIO_InitStruct;
+		GPIO_InitStruct.Pin = GPIO_PIN_DIST_ECHO;
+		GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	}
+}
+
+static void vTriggerMeasurementTask(void *pvParameters)
+{
 	for (;;) {
-		i++;
-		taskENTER_CRITICAL();
-		loggerPrintf("L%d: a!", i);
-		taskEXIT_CRITICAL();
-		vTaskDelay(100);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-		vTaskDelay(400);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_DIST_TRIGGER, GPIO_PIN_RESET);
+		vTaskDelay(1);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_DIST_TRIGGER, GPIO_PIN_SET);
+		vTaskDelay(1);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_DIST_TRIGGER, GPIO_PIN_RESET);
+//		taskENTER_CRITICAL();
+//		loggerPrintf("%d: trig meas", xTaskGetTickCount());
+//		taskEXIT_CRITICAL();
+		vTaskDelay(500);
+//              vTaskDelay(100);
+//              HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+//              vTaskDelay(400);
+//              HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+	}
+}
+
+static void vMeasureTask(void *pvParameters)
+{
+	for (;;) {
+		TickType_t tickAtRaising, tickAtFalling;
+		GPIO_PinState s1 = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_DIST_ECHO);
+		vTaskDelay(1);
+		GPIO_PinState s2 = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_DIST_ECHO);
+		if (s1 == GPIO_PIN_RESET && s2 == GPIO_PIN_SET) {
+			tickAtRaising = xTaskGetTickCount();
+			tickAtFalling = 0;
+//                      taskENTER_CRITICAL();
+//                      loggerPrintf("%d: slope raising", xTaskGetTickCount());
+//                      taskEXIT_CRITICAL();
+		} else if (s1 == GPIO_PIN_SET && s2 == GPIO_PIN_RESET) {
+			tickAtFalling = xTaskGetTickCount();
+			taskENTER_CRITICAL();
+			loggerPrintf("%d: slope falling, (%d - %d) * C = %dm",
+				     xTaskGetTickCount(), tickAtFalling,
+				     tickAtRaising,
+				     343 * (tickAtFalling -
+					    tickAtRaising) / 1000);
+			taskEXIT_CRITICAL();
+			tickAtRaising = 0;
+			tickAtFalling = 0;
+		}
 	}
 }
 
@@ -101,8 +171,9 @@ static void SystemClock_Config(void)
 	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
 	/* Enable HSI Oscillator and activate PLL with HSI as source */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_LSI;
 	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.LSIState = RCC_LSI_ON;
 	RCC_OscInitStruct.HSICalibrationValue = 0x10;
 	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
 	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
@@ -110,9 +181,12 @@ static void SystemClock_Config(void)
 	RCC_OscInitStruct.PLL.PLLN = 336;
 	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
 	RCC_OscInitStruct.PLL.PLLQ = 7;
+	loggerPrintf("SystemClock_Config 0");
 	if ((status = HAL_RCC_OscConfig(&RCC_OscInitStruct)) != HAL_OK) {
+		loggerPrintf("SystemClock_Config error");
 		Error_Handler();
 	}
+	loggerPrintf("SystemClock_Config 0 pass");
 
 	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
 	   clocks dividers */
@@ -126,6 +200,57 @@ static void SystemClock_Config(void)
 	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
 		Error_Handler();
 	}
+	loggerPrintf("SystemClock_Config bye");
+}
+
+static void RTC_Config(void)
+{
+	loggerPrintf("rtc config 0");
+	RTC_HandleTypeDef RtcHandle;
+	RtcHandle.Instance = RTC; 
+	RtcHandle.Init.HourFormat = RTC_HOURFORMAT_24;
+	RtcHandle.Init.AsynchPrediv = RTC_ASYNCH_PREDIV;
+	RtcHandle.Init.SynchPrediv = RTC_SYNCH_PREDIV;
+	RtcHandle.Init.OutPut = RTC_OUTPUT_DISABLE;
+	RtcHandle.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+	RtcHandle.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+
+	loggerPrintf("rtc config 1");
+  	HAL_StatusTypeDef ret = HAL_RTC_Init(&RtcHandle);
+	loggerPrintf("rtc config 2");
+	switch (ret)
+	{
+		case HAL_OK:
+			loggerPrintf("HAL_OK");
+			break;
+		case HAL_ERROR:
+			loggerPrintf("HAL_ERROR");
+			break;
+		case HAL_BUSY:
+			loggerPrintf("HAL_BUSY");
+			break;
+		case HAL_TIMEOUT:
+			loggerPrintf("HAL_TIMEOUT");
+			break;
+		default:
+			loggerPrintf("HAL_???");
+			break;
+	}
+
+  	if(ret != HAL_OK)
+	{
+		Error_Handler(); 
+	}
+
+//
+//	// 0000000000000000000000000000000000]]]
+//    RTC_DateTypeDef sdatestructureget;
+//    RTC_TimeTypeDef stimestructureget;
+//    
+//    /* Get the RTC current Time */
+//    HAL_RTC_GetTime(&RtcHandle, &stimestructureget, FORMAT_BIN);
+//    /* Get the RTC current Date */
+//    HAL_RTC_GetDate(&RtcHandle, &sdatestructureget, FORMAT_BIN);
 
 }
 
